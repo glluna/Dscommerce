0967b99ca4236087db92395371b9abc447f27d97
glluna <gabrielluna98@hotmail.com>
2025-03-13 20:50:24 -0300

    GET Products paged

--- src/main/java/br/com/luna/controller/ProductController.java
+++ src/main/java/br/com/luna/controller/ProductController.java
@@ -1,15 +1,16 @@
 package br.com.luna.controller;
 
-import java.util.Optional;
+
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.luna.dto.ProductDTO;
-import br.com.luna.entities.Product;
 import br.com.luna.services.ProductService;
 
 @RestController
@@ -21,9 +22,12 @@ public class ProductController {
 	
 	@GetMapping(value = "/{id}")
 	public ProductDTO findById(@PathVariable Long id) {
-		return service.findById(id);
-
-	    
+		return service.findById(id);   
+	}
+	
+	@GetMapping
+	public Page<ProductDTO> findAll(Pageable pageable) {
+		return service.findAll(pageable);	    
 	}
 	
 }
